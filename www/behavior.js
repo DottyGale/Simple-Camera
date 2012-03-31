3f025414f742823d0bd1978e3f5ba5a91b3aa2ed
Eric Gelinas <eric@Eric-Gelinass-MacBook.local>
2012-03-31 12:52:07 -0700

    a simple ui scaffold

---
+++ www/behavior.js
@@ -0,0 +1,24 @@
+(function() {
+	
+	/*
+	*  Simple camera behavior
+	*/
+	
+	window.SCA = {}; //Simple Camera App
+	
+	window.SCA = (function() {
+		
+		var out = null;
+		    
+		
+		out = {
+			
+			version : '0.1.0'
+			
+		}
+		
+		return out;
+		
+	})();
+	
+})();
