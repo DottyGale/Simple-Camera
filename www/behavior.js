9af19866c3cca4d0d436f0003f8cdc2e6d92ffcb
Eric Gelinas <eric@standardpixel.com>
2012-03-31 14:31:44 -0700

    add a screen manager

--- www/behavior.js
+++ www/behavior.js
@@ -6,14 +6,59 @@
 	
 	window.SCA = {}; //Simple Camera App
 	
+	var SCREENS_SELECTOR = '.simple-camera div.content section';
+	
 	window.SCA = (function() {
 		
 		var out = null;
-		    
+		
+		function getDomScreens() {
+			
+			var elements  = document.querySelectorAll(SCREENS_SELECTOR),
+			    map       = {};
+			
+			for(var i=0, l=elements.length; l > i; i++) {
+				var element = elements[i];
+				map[element.dataset['key'] || i] = element;
+			}
+			
+			return map;
+			
+		}   
 		
 		out = {
 			
-			version : '0.1.0'
+			_screens : null,
+			
+			init : function() {
+				
+				this._screens = getDOMScreens();
+				
+			},
+			
+			getAllScreens : function() {
+				return this._screens;
+			},
+			
+			removeScreen : function(key) {
+				var element = null;
+				
+				for(var i=0 in this._screens) {
+					if(this._screens.hasOwnProperty(i) && i===key) {
+						element = this._screens[i];
+						element.parentNode.removeChild(element);
+						delete this._screens[i];
+						
+						return !this._screens[i];
+					}
+				}
+				
+				return false;
+			},
+			
+			appendScreen : function(key, element) {
+				this._screens[key] === element;
+			}
 			
 		}
 		
